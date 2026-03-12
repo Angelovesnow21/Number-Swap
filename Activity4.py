f21aaa8eecc133f34e997f868afad959be6f1e74
Angelovesnow21 <jessica.elaine8468@gmail.com>
2026-03-12 21:10:43 +0600

    Add files via upload

---
+++ Activity4.py
@@ -0,0 +1,12 @@
+# Take input values from user
+a = input("Enter Value of a:")
+b = input("Enter Value of b:")
+
+# Swapping
+temp = a
+a = b
+b = temp
+
+# Displaying results after swapping
+print("value of a after swapping", a)
+print("value of b after swapping", b)
